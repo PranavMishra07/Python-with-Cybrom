3607c3c25f93c3e00f88051048b5822931fb6efe
PranavMishra07 <pranav072003@gmail.com>
2025-02-26 20:02:27 +0530

    a

---
+++ Airithmatic.py
@@ -0,0 +1,20 @@
+a=10
+b=3
+c=a+b
+print(c)
+c=a-b
+print(c)
+c=a*b
+print(c)
+c=a/b
+print(c)
+c=a//b
+print(c)
+c=a%b
+print(c)
+
+
+#write a program to find area of all the Shapes  input taken from user
+#relational operation always return boolean Value there are 6 
+# find the roots of quadric equation also show the roots are equal or not equal
+# Conditional Statement if else
--- first.py
+++ first.py
@@ -1,12 +1,22 @@
-print("hello World!")
-print(2+2)
-print(2*4)
-a=23
-print(id(a))
-print(type(a))
-a=20
-print(id(a))
-print(a)
-a=2.2
-print(type(a))
-print(a*a)
+#python is a high level ,object oriented ,open source ,interpreted,syntexless,indentloosed,generalpurpose,platform-independent,Dynamic Memory allocation and easy to learn Language
+# print("hello World!")
+# print(2+2)
+# print(2*4)
+# a=23
+# print(id(a))
+# print(type(a))
+# a=20
+# print(id(a))
+# print(a)
+# a=2.2
+# print(type(a))
+# print(a*a)
+# a=5
+# a+=2
+# print(a)
+# write a program to swap two variable without using third variable and without using arithmetic opration
+a=int(input("Enter The First No : "))
+b=int(input("Enter The Second No : "))
+print(f"Before Swapping The value of a={a} and b={b}")   #fstring -formating string
+a,b=b,a
+print(f"After Swapping The value of a={a} and b={b}")   #fstring -formating string
