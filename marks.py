da3d5ab1f5118a9b218be6575287d386a8deda3f
PranavMishra07 <pranav072003@gmail.com>
2025-02-28 20:01:20 +0530

    dd

---
+++ marks.py
@@ -0,0 +1,10 @@
+hindi=int(input("Enter Hindi Marks : "))
+English=int(input("Enter English Marks : "))
+maths=int(input("Enter maths Marks : "))
+
+if(hindi>33):
+    print("Marks is {hindi} Pass")
+elif(hindi<33):
+    print("{hindi} Fail")
+elif(English>33):
+    print("")
