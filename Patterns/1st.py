e231e2e0fbd38e32951e943751a9c5697f21fedf
PranavMishra07 <pranav072003@gmail.com>
2025-03-08 12:16:56 +0530

    sdf

--- Patterns/1st.py
+++ Patterns/1st.py
@@ -16,3 +16,124 @@
 #     print()
 
 
+# for r in range(1,6): # r=1,2,3,4,5
+#     for c in range(1,r+1):          #1,2,3,4,5
+#         print("*",end=" ")
+#     print()
+
+
+# for r in range(6,0,-1):
+#     for c in range(1,r+1):
+#         print("*",end=" ")
+#     print()
+
+# for r in range (1,6):
+#     for c in range(1,r+1):
+#         print(r,end=" ")
+#     print()
+
+# for r in range (6,1,-1):
+#     for c in range(1,r):
+#         print(c,end=" ")
+#     print()
+
+# n=1
+# for r in range (1,6):   
+#     for c in range(1,r+1):        
+#         print(n,end=" ")  
+#         n+=1  
+#     print()
+
+
+# for r in range (1,6):
+#     for c in range(1,r+1):
+#         print(c,end=" ")
+#     print()
+
+# for r in range(6,1,-1):
+#     for c in range(1,r):
+#         print(c,end=" ")
+#     print()
+
+
+# for r in range(1,6):
+#     for c in range(1,r+1):
+#         print(chr(64+r),end=" ")
+#     print()
+
+# for r in range(1,6):
+#     for c in range(1,r+1):
+#         print(chr(64+c),end=" ")
+#     print()
+
+
+# n=1
+# for r in range(1,8):
+#     for c in range(1,r+1):
+#         print(chr(64+n),end=" ")
+#         n+=1
+#     print()
+
+# for r in range(1,6):
+#     for c in range(6,r,-1):
+#         print(" ",end=" ")
+#     for c in range(1,r+1):
+#           print("*",end=" ")
+#     print()
+
+# n=1
+# for r in range(1,6):
+#     for c in range(6,r,-1):
+#         print(" ",end=" ")
+#     for c in range(1,r+1):
+#         print(chr(64+n),end=" ")
+#         n+=1
+#     print()
+
+
+# for r in range(1,6):
+#     for c in range(1,r+1):
+#         print((r+c)%2,end=" ")
+#     print()
+
+
+# for r in range(1,6):
+#     for c in range(1,r+1):
+#         if (r+c)%2==0 :
+#             print("0",end=" ")
+#         else:
+#             print("1",end=" ")
+#     print()
+
+
+
+# for r in range(1,6):
+#     for c in range(6,r,-1):
+#         print(" ",end=" ")
+#     for c in range(1,2*r):
+#         print("*",end=" ")
+#     print()
+#           * 
+#         * * *
+#       * * * * *
+#     * * * * * * *
+#   * * * * * * * * *
+
+
+
+# for r in range(5,0,-1):
+#     for c in range(6,r,-1):
+#         print(" ",end=" ")
+#     for c in range(1,2*r):
+#         print("*",end=" ")
+#     print()
+#   * * * * * * * * * 
+#     * * * * * * *
+#       * * * * *
+#         * * *
+#           *
+
+
+
+
+for r in range
