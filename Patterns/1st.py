58ad258535c7ddc7ab4f6911a45a7753fefcf32b
PranavMishra07 <pranav072003@gmail.com>
2025-04-06 18:10:59 +0530

    Update 1st.py

--- Patterns/1st.py
+++ Patterns/1st.py
@@ -339,3 +339,11 @@ for row in range(7):
             print(" ",end=" ")
                   
     print()
+   for row in range(7):
+    for col in range(5):
+        if(row==0 or row==3) or (col==0 or col==4):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+                  
+    print()
