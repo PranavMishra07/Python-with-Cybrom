201831275ccafe53f38a9d709c8c6b8a87473f4a
PranavMishra07 <pranav072003@gmail.com>
2025-04-07 15:00:34 +0530

    Create  magicmathod.py

---
+++ OOPS/magicmathod.py
@@ -0,0 +1,8 @@
+class myclass:
+    def __init__(self,para):
+        self.para=para
+    def __gt__(self,other):
+        return self.para>other.para
+ob=myclass(300)
+ob1=myclass(100)
+print(ob>ob1)
